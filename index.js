7be4d5fca1395073d69fa5bee30688f28033fcc0
Andrea Pallotta <ap4534@rit.edu>
2021-10-13 15:11:07 -0400

    Rewriting socket.io logic

--- index.js
+++ index.js
@@ -1,36 +1,56 @@
-const uuid = require('uuid').v5;
-const express = require('express');
-const color = require('colors');
-const cors = require('cors');
-const config = require('./configs/configs')();
-const db = require('./utilities/mongoConnect');
-const { NEW_GLOBAL_CHAT_MESSAGE, GLOBAL_CHAT } = require('./utilities/endpoints');
+const uuid = require("uuid").v5;
+const express = require("express");
+const color = require("colors");
+const cors = require("cors");
+const config = require("./configs/configs")();
+const db = require("./utilities/mongoConnect");
+const endpoints = require("./utilities/endpoints");
+const {
+  addUser,
+  removeUser,
+  getUsersInRoom,
+} = require("./utilities/chatHandler");
+const { getMessagesInRoom } = require("./utilities/messageStorage");
 
 const app = express();
 app.use(cors());
 const server = app.listen(config.app.port, config.app.localhost, () => {
-    console.log(`${config.app.localhost} running on port ${config.app.port}`);
+  console.log(`${config.app.localhost} running on port ${config.app.port}`);
 });
 
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-        methods: ['Get', 'POST'],
-    },
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["Get", "POST"],
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log(`new client ${socket.id} connected`);
+io.on("connection", (socket) => {
+  console.log(`new client connected`);
+  const { roomId, name } = socket.handshake.query;
+  console.log(socket.handshake.query)
+  socket.join(roomId);
 
-    socket.join(GLOBAL_CHAT);
+  const user = addUser(socket.id, roomId, name);
+  io.in(roomId).emit(endpoints.USER_JOIN_CHAT_EVENT, user);
 
-    socket.on(NEW_GLOBAL_CHAT_MESSAGE, (data) => {
-        io.in(GLOBAL_CHAT).emit(NEW_GLOBAL_CHAT_MESSAGE, data);
-    });
+  socket.on(endpoints.NEW_GLOBAL_CHAT_MESSAGE, (data) => {
+    io.in(roomId).emit(endpoints.NEW_GLOBAL_CHAT_MESSAGE, data);
+  });
 
-    socket.on('disconnect', () => {
-        socket.leave(GLOBAL_CHAT);
-    });
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    io.in(roomId).emit(endpoints.USER_LEAVE_CHAT_EVENT, user);
+    socket.leave(roomId);
+  });
 });
 
+app.get("/rooms/:roomId/users", (req, res) => {
+  const users = getUsersInRoom(req.params.roomId);
+  return res.json({ users });
+});
 
+app.get("/rooms/:roomId/messages", (req, res) => {
+  const messages = getMessagesInRoom(req.params.roomId);
+  return res.json({ messages });
+});
---
+++ utilities/chatHandler.js
@@ -0,0 +1,26 @@
+const users = [];
+
+const addUser = (id, room, name) => {
+  const foundUser = users.find(
+    (user) => user.room === room && user.name === name
+  );
+
+  const user = { id, name, room };
+  users.push(user);
+  return { id, name: user.name };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) return users.splice(index, 1)[0];
+};
+
+const getUser = (id) => users.find((user) => (user.id = id));
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+};
