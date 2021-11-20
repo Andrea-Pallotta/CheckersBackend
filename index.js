064391d2304f24be6412a01e1e3dfe60f7041c92
Andrea Pallotta <ap4534@rit.edu>
2021-11-19 23:20:32 -0500

    ci/cd test

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ const server = app.listen(config.app.port, config.app.localhost, () => {
 });
 
 const io = require("socket.io")(server, {
-  "reconnection limit": 1000,
-  "max reconnection attempts": 20,
   parser: require("socket.io-msgpack-parser"),
   cors: {
     origin: "*",
