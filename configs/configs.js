03e234576380f7aa9bbf877f9ecd769c98d9ced2
Andrea Pallotta <ap4534@rit.edu>
2021-11-21 12:50:13 -0500

    Got rid of the .env file

--- configs/configs.js
+++ configs/configs.js
@@ -1,24 +1,10 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = () => ({
-    app: {
-        name: process.env.APP_NAME,
-        host: process.env.APP_HOST || '192.168.1.10',
-        localhost: process.env.APP_LOCALHOST || '127.0.0.1',
-        port: process.env.APP_PORT || 5050,
-        env: process.env.APP_ENV,
-        logspath: process.env.LOG_PATH,
-    },
-    mongodb: {
-        uri: process.env.DB_URI,
-        host: process.env.DB_HOST,
-    },
-    logging: {
-        file: process.env.LOG_PATH,
-        level: process.env.LOG_LEVEL || 'info',
-        console: process.env.LOG_ENABLE_CONSOLE || true,
-    },
-    cookies: {
-        secret: process.env.COOKIE_SECRET,
-    }
+  app: {
+    name: "Connect4 Server",
+    host: process.env.NODE_ENV === "production" ? "172.31.17.228" : "127.0.0.1",
+    port: 5050,
+    logspath: "./logs/logs.txt",
+  },
 });
