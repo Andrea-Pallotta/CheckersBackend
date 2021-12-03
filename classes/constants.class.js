f276e2a07e5d28bfb93388e55b3aa58fb0ed1006
Andrea Pallotta <ap4534@rit.edu>
2021-12-02 22:34:14 -0500

    Fixed constants

--- classes/constants.class.js
+++ classes/constants.class.js
@@ -1,36 +1,45 @@
 const WaitQueue = require('wait-queue');
 const Game = require('./game.class');
 
-class Constants {
-  static NEW_GAME_BOARD = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-  ];
-  static SOCKETS = new Map();
-  static GLOBAL = new Map();
-  static GAMES = new Map();
-  static MESSAGES = new Set();
-  static QUEUE = new WaitQueue();
-  static GAMECOUNT = 0;
+const NEW_GAME_BOARD = [
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+];
+const SOCKETS = new Map();
+const GLOBAL = new Map();
+const GAMES = new Map();
+const MESSAGES = new Set();
+const QUEUE = new WaitQueue();
+const GAMECOUNT = 0;
 
-  static INITIAL_GAME_STATE = (player1, player2, roomId) => {
-    return new Game(
-      this.NEW_GAME_BOARD,
-      player1,
-      player2,
-      1,
-      { x: undefined, y: undefined },
-      roomId,
-      `It's ${player1.username} turn.`,
-      false,
-      undefined
-    );
-  };
-}
+const INITIAL_GAME_STATE = (player1, player2, roomId) => {
+  return new Game(
+    this.NEW_GAME_BOARD,
+    player1,
+    player2,
+    1,
+    { x: undefined, y: undefined },
+    roomId,
+    `It's ${player1.username} turn.`,
+    false,
+    undefined
+  );
+};
+
+const Constants = {
+  NEW_GAME_BOARD,
+  SOCKETS,
+  GLOBAL,
+  GAMES,
+  MESSAGES,
+  QUEUE,
+  GAMECOUNT,
+  INITIAL_GAME_STATE,
+};
 
 module.exports = Constants;
